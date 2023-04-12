00ad36c3340ab2d23bb90c5edaab2d333e627103
nicola00 <nicolajobs00@gmail.com>
2023-04-12 19:26:20 +0200

    FEAT style card add arrows

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <script src="https://kit.fontawesome.com/5ffb897fe9.js" crossorigin="anonymous"></script>
     <title>Laravel</title>
 
     <!-- Fonts -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,25 +4,52 @@
 @section('content')
 
 <div class="container">
-    <h1>ciao a tutti i treni</h1>
-    @foreach ($trains as $train)
-    <div class="grid">
+    <h1 class="py-4">ciao a tutti i treni</h1>
+
+    <div class="grid-train">
+        @foreach ($trains as $train)
+
         <div class="card-train">
             <h3>
-                azienda
+                {{ $train->Azienda }}
             </h3>
-            <p>stazione di arrivo</p>
-            <p>stazione di partenza</p>
-            <p>orario di partenza</p>
-            <p>orario di arrivo</p>
+            <div class="row-stasion">
+                <p class="py-3 m-0">{{ $train['stazione di arrivo'] }}</p>
+                <i class="fa-solid fa-right-left"></i>
+                <p class="py-3 m-0">{{ $train['stazione di partenza'] }}</p>
+            </div>
+            <div class="row-hour">
+                <p class="py-3 m-0">{{ $train['orario di partenza'] }}</p>
+                <i class="fa-solid fa-right-left"></i>
+                <p class="py-3 m-0"> {{ $train['orario di arrivo'] }}</p>
+            </div>
         </div>
+        @endforeach
+
     </div>
 
-    @endforeach
 </div>
 
 <style>
-    .grid {}
+    .grid-train {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 20px;
+    }
+
+    .card-train {
+        border: 3px solid red;
+        padding: 15px;
+        border-radius: 8px;
+    }
+
+    .row-stasion,
+    .row-hour {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: nowrap;
+        align-items: center;
+    }
 </style>
 
 @endsection
