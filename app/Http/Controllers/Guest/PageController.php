3caaad29a5c12e7371faab1554c25927a36587c7
nicola00 <nicolajobs00@gmail.com>
2023-04-12 17:39:35 +0200

    FEAT create model and controller

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+
+class PageController extends Controller
+{
+    public function homePage(){
+        $trains = Train::all();
+
+        $data=[
+            'trains' => $trains
+        ];
+
+        return view ('welcome', $data)
+    }
+}
